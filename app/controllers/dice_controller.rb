611141344b5caea4d14703c1426c29fa56c3ca8f
mkannapally <akannapa@chicagobooth.edu>
2022-10-25 23:15:54 +0000

    dynamic roll fully working!

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,5 +1,20 @@
 class DiceController < ApplicationController
 
+
+  def infinity_and_beyond
+    @num_dice = params.fetch("number_of_dice").to_i
+    @num_faces = params.fetch("how_many_sides").to_i
+
+    @array_of_rolls = Array.new
+
+    @num_dice.times do 
+      @array_of_rolls.push(rand(@num_faces) + 1)
+    end
+
+    render({:template => "dice_templates/infinity_and_beyond.erb"})
+  end
+
+
   def one_six
     @array_of_rolls = Array.new
 
--- config/routes.rb
+++ config/routes.rb
@@ -28,8 +28,11 @@ Rails.application.routes.draw do
   # PART 3: MORE R→C→A→V PRACTICE
   # ==========================
     
+  get("/roll/:number_of_dice/:how_many_sides" , { :controller => "dice", :action => "infinity_and_beyond"})
+
   get("/roll/1/6", { :controller => "dice", :action => "one_six" })
   get("/roll/2/6", { :controller => "dice", :action => "two_six" })
+
   get("/roll/3/6", { :controller => "dice", :action => "three_six" })
   get("/roll/4/6", { :controller => "dice", :action => "four_six" })
   get("/roll/5/6", { :controller => "dice", :action => "five_six" })
@@ -50,4 +53,5 @@ Rails.application.routes.draw do
   get("/roll/1/20", { :controller => "dice", :action => "one_twenty" })
   get("/roll/2/20", { :controller => "dice", :action => "two_twenty" })
   get("/roll/3/20", { :controller => "dice", :action => "three_twenty" })
+  
 end
